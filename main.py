ccaaebcc6118f50b35aebc21b96d5700dc19bf5c
katrinakit <149562106+katrinakit@users.noreply.github.com>
2024-01-24 10:24:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,85 @@
 # Github Fork (repozitorija kopija) - https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo
 # Klonēt repozitoriju - hhttps://code.visualstudio.com/docs/sourcecontrol/intro-to-git
 
+
+
+
+
+
+print("katrīnas Lauras Terentjevas spēle cirks")
+print("sākam spēli, vēlu veiksmi!")
+print("player 1 met kauliņu, un iet uz priekšu")
+import random 
+x = (random.randint(1, 6))
+print(x)
+print("player 2 met kauliņu, un iet uz priekšu")
+import random
+q = (random.randint(1, 6)) 
+print (q)
+print("player 1 met kauliņu, un iet uz priekšu")
+import random
+y = (random.randint(1, 6))
+print(y)
+w = (x + y)
+print("player 1 atrašanās vieta ir", w)
+print("player 2 met kauliņu, un iet uz priekšu")
+import random
+e = (random.randint(1, 6))
+print(e)
+r = (q + e)
+print("player 2 atrašanās vieta ir", r)
+print("player 1 met kauliņu, un iet uz priekšu")
+import random
+t = (random.randint(1, 6))
+print(t)
+u = (t + w)
+if u == 15:
+     print("jūs ejat pa sarkanajām trepēm uz 24")
+if not u == 15: 
+     print ("player 1 atrašanās vita ir", u)
+print("player 2 met kauliņu, un iet uz priekšu")
+import random
+i = (random.randint(1, 6))
+print(i)
+o = (i + r)
+if o == 15:
+     print("jūs ejat pa sarkanajām trepēm uz 24")
+if not o == 15: 
+     print ("player 2 atrašanās vita ir", o)
+print("player 1 met kauliņu, un iet uz priekšu")
+import random
+p = (random.randint(1, 6))
+print(p)
+a = (u + p)
+if a == 15:
+     print("jūs ejat pa sarkanajām trepēm uz 24")
+if a == 18:
+     print("jūs ejat pa zilajām trepēm atpakaļ uz 7")
+if not a == 15 or a == 18: 
+     print ("player 1 atrašanās vieta ir", a)
+     print("player 2 met kauliņu, un iet uz priekšu")
+import random
+s = (random.randint(1, 6))
+print(s)
+d = (o + s)
+if d == 15:
+     print("jūs ejat pa sarkanajām trepēm uz 24")
+if d == 18:
+     print("jūs ejat pa zilajām trepēm atpakaļ uz 7")
+if not d == 15 or d == 18: 
+     print ("player 2 atrašanās vieta ir", d)
+     print("player 1 met kauliņu, un iet uz priekšu")
+import random
+f = (random.randint(1, 6))
+print(f)
+g = (a + f)
+if g == 15:
+     print("jūs ejat pa sarkanajām trepēm uz 24")
+if g == 18:
+     print("jūs ejat pa zilajām trepēm atpakaļ uz 7")
+if not g == 15 or g == 18: 
+     print ("player 1 atrašanās vieta ir", g)
+     
+
+
+
